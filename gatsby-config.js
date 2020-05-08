7fa3922cb5dfae1fad2d6c483bffb6b1fd5a23df
Amie Wilt <amie.wilt@gmail.com>
2020-05-08 10:11:01 -0400

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,18 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "Better Blog POC",
+    description: "POC for better blog",
+  },
+  plugins: [
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,26 @@
 import React from "react"
+import { StaticQuery, graphql } from "gatsby"
 
-export default () => <div>Hello world!</div>
+const Header = () => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          site {
+            siteMetadata {
+              title
+              description
+            }
+          }
+        }
+      `}
+      render={data => <div>{data.site.siteMetadata.title}</div>}
+    />
+  )
+}
+
+const Layout = () => {
+  return <Header />
+}
+
+export default Layout
